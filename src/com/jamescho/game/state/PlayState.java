b6206c2159199939e4082d3f68b06f58afd1f5a9
Chris Ortman <chris-ortman@uiowa.edu>
2017-03-21 21:37:10 -0500

    Adding PlayState with default background

--- src/com/jamescho/game/state/PlayState.java
+++ src/com/jamescho/game/state/PlayState.java
@@ -1,5 +1,6 @@
 package com.jamescho.game.state;
 
+import com.jamescho.game.main.GameMain;
 import com.jamescho.game.main.Resources;
 
 import java.awt.*;
@@ -7,12 +8,12 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
 /**
- * Created by cortman on 2/19/17.
+ * Created by cortman on 3/21/17.
  */
-public class MenuState extends State {
+public class PlayState extends State {
     @Override
     public void init() {
-        System.out.println("Entered MenuState");
+
     }
 
     @Override
@@ -22,17 +23,24 @@ public class MenuState extends State {
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Resources.welcome,0,0,null );
+        //Background
+        g.setColor(Resources.darkBlue);
+        g.fillRect(0,0, GameMain.GAME_WIDTH, GameMain.GAME_HEIGHT);
+        g.setColor(Resources.darkRed);
+        g.fillRect(GameMain.GAME_WIDTH/2, 0,GameMain.GAME_WIDTH/2, GameMain.GAME_HEIGHT);
+
+        //line
+        g.drawImage(Resources.line, (GameMain.GAME_WIDTH / 2) - 2, 0, null);
     }
 
     @Override
     public void onClick(MouseEvent e) {
-        setCurrentState(new PlayState());
+
     }
 
     @Override
     public void onKeyPress(KeyEvent e) {
-        System.out.println("Pressed the " + e.getKeyChar() + " key");
+
     }
 
     @Override
