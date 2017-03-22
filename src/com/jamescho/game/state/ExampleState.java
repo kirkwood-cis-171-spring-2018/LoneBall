6e081b267fdda4e6e5e3544d38857f06aac9e041
Chris Ortman <chris-ortman@uiowa.edu>
2017-03-21 22:14:39 -0500

    Example state to demonstrate animation

--- src/com/jamescho/game/state/ExampleState.java
+++ src/com/jamescho/game/state/ExampleState.java
@@ -1,18 +1,18 @@
 package com.jamescho.game.state;
 
-import com.jamescho.game.main.Resources;
-
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
 /**
- * Created by cortman on 2/19/17.
+ * Created by cortman on 3/21/17.
  */
-public class MenuState extends State {
+public class ExampleState extends State {
+
+
     @Override
     public void init() {
-        System.out.println("Entered MenuState");
+
     }
 
     @Override
@@ -22,17 +22,19 @@ public class MenuState extends State {
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Resources.welcome,0,0,null );
+        g.setColor(Color.red);
+        g.drawRect(400, 200, 10, 15);
     }
 
     @Override
     public void onClick(MouseEvent e) {
-        setCurrentState(new ExampleState());
+
     }
 
     @Override
     public void onKeyPress(KeyEvent e) {
-        System.out.println("Pressed the " + e.getKeyChar() + " key");
+
+
     }
 
     @Override
