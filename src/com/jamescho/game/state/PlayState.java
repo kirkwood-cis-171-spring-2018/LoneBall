aa754fc23188f1997fc000465cb77aabc450a681
jacob steinman <jacobsteinman@jacobs-MacBook-Air.local>
2018-03-22 11:02:55 -0500

    i made my paddles

--- src/com/jamescho/game/state/PlayState.java
+++ src/com/jamescho/game/state/PlayState.java
@@ -11,6 +11,16 @@ import java.awt.event.MouseEvent;
  * Created by cortman on 3/21/17.
  */
 public class PlayState extends State {
+    private int xPos = 0;
+    private int yPos = GameMain.GAME_HEIGHT / 2;
+    private int xPos2 = GameMain.GAME_WIDTH - 5;
+    private int yPos2 = GameMain.GAME_HEIGHT / 2;
+    private int endPositionX = 0;
+    private int endPositionY = 0;
+    private boolean moveRight = true;
+    private int yVelocity;
+
+
     @Override
     public void init() {
 
@@ -18,7 +28,8 @@ public class PlayState extends State {
 
     @Override
     public void update() {
-
+        yPos += yVelocity;
+        yPos2 -= yVelocity;
     }
 
     @Override
@@ -31,20 +42,46 @@ public class PlayState extends State {
 
         //line
         g.drawImage(Resources.line, (GameMain.GAME_WIDTH / 2) - 2, 0, null);
+
+
+        g.setColor(Color.white);
+        g.fillRect(xPos,yPos,5,50);
+
+        g.setColor(Color.ORANGE);
+        g.fillRect(xPos2,yPos2,5,50);
     }
 
     @Override
     public void onClick(MouseEvent e) {
+        boolean doubleClick = false;
+        if (e.getClickCount() == 2) {
+            doubleClick = true;
+        }
+        if (doubleClick) {
+            setCurrentState(new PlayState());
+        }
+
 
     }
 
     @Override
     public void onKeyPress(KeyEvent e) {
-
+        char pressedKey = e.getKeyChar();
+        pressedKey = Character.toLowerCase(pressedKey);
+        System.out.println(pressedKey);
+        if (pressedKey == 'w' && pressedKey == 's') {//program crashes after a while checking if this was the problem I doubt it
+            System.out.println("for some reason the program read both w & s");
+        }//end of useless info
+        if (pressedKey == 'w') {
+            yVelocity = -5;
+        } if (pressedKey == 's') {
+            yVelocity = 5;
+        }
     }
 
     @Override
     public void onKeyRelease(KeyEvent e) {
 
+        yVelocity = 0;
     }
 }
